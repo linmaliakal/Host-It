df8a4008637e7fb1a9f6e95ed015ed41c5d5a20c
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-10-31 15:36:52 -0500

    added more to core structure and files

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,1 +1,28 @@
 //Home Screen
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight } from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.ViewContainer}>
+        <Text>HomeScreen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create(
+  {
+    ViewContainer:
+    {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+    },
+  });
